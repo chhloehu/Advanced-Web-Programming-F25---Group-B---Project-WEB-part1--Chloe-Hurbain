0b995326a0c7063026fd59327685a9103e2aa8b5
chhloehu <151128443+chhloehu@users.noreply.github.com>
2025-10-28 10:58:05 -0400

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,34 @@
+/* 
+  data.js contient la liste des catastrophes naturelles.
+  Chaque objet représente une catastrophe avec :
+    - title : le nom
+    - description : description courte
+    - img : image illustrative 
+*/
+const disasters = [
+  {
+    title: "Earthquake",
+    description: "A sudden shaking of the ground caused by tectonic plate movements. Can destroy buildings and infrastructure.",
+    img: "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202302/MIT-Earthquake-relief-01_0.jpg?itok=pXSA4o3G",
+  },
+  {
+    title: "Flood",
+    description: "Overflow of water onto normally dry land. Can cause significant damage to homes and agriculture.",
+    img: "https://ichef.bbci.co.uk/news/1024/cpsprodpb/daff/live/e56fd7e0-af88-11ef-bb6c-fd7d385cab80.jpg.webp",
+  },
+  {
+    title: "Hurricane",
+    description: "A powerful tropical storm with strong winds and heavy rainfall. Can lead to destruction and economic loss.",
+    img: "https://www.nesdis.noaa.gov/s3/styles/webp/s3/migrated/20180914-HURFlorenceC0745EST01.jpg.webp?itok=pvoMIYbM",
+  },
+  {
+    title: "Drought",
+    description: "A prolonged period of low rainfall causing water scarcity and agricultural impacts.",
+    img: "https://wmo.int/sites/default/files/styles/prose_2x/public/2023-03/drought-1675729_640.jpg?itok=D4eYHVbN",
+  },
+  {
+    title: "Volcanic Eruption",
+    description: "Explosion of magma from a volcano. Can destroy villages and affect climate with lava flows and ash clouds.",
+    img: "https://ichef.bbci.co.uk/images/ic/976xn/p06lfdtq.jpg",
+  }
+];
